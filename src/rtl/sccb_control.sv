746e9db5bd600eecbaa29c6eee3fd0004f503452
Swarup Majumder <sumajum@gmail.com>
2025-11-18 00:52:06 -0600

    Simpler Control Unit but need to drive SCL slower

--- src/rtl/sccb_control.sv
+++ src/rtl/sccb_control.sv
@@ -22,7 +22,7 @@
 
 module sccb_control(
         //clock!, sda, scl inputs, outputs as well. 
-        input logic clk,
+        input logic xclk,
         
 
         input logic start_fsm,
@@ -33,339 +33,132 @@ module sccb_control(
     );
 
     
-    enum logic [10:0] { 
+    enum logic [2:0] { 
         s_start,
-        s_write_7,
-        s_write_7_2,
-        s_write_7_3,
-
-
-        s_write_6,
-        s_write_6_2,
-        s_write_6_3,
-      
-      
-        s_write_5,
-        s_write_5_2,
-        s_write_5_3,
-
-        s_write_4,
-        s_write_4_2,
-        s_write_4_3,
-
-        s_write_3,
-        s_write_3_2,
-        s_write_3_3,
-
-        s_write_2,
-        s_write_2_2,
-        s_write_2_3,
-
-        s_write_1,
-        s_write_1_2,
-        s_write_1_3,
-
-        s_write_0,
-        s_write_0_2,
-        s_write_0_3,
-
-        ack,
-        s_idle
+        s_write,
+        s_idle,
+        s_ack,
+        s_stop
     } curr_state, next_state;
 
+    logic [15:0] dout;
     logic [7:0] reg_addr;
     logic [7:0] reg_data;
-    logic [15:0] dout;
-    logic [7:0] counter;
-
-    logic addr_or_data; //not doing anything with this rn
     
-
+    
+    
+    
+    //BASED ON ROM FORMAT
     assign reg_addr = dout[15:8];
     assign reg_data = dout[7:0];
 
+
+    //HARD CODED ADDR 0x42 TO WRITE TO
+    logic [7:0] addr_42;
+    assign addr_42 = 8'h42;
+
+    //COUNTERS
+    logic [6:0] rom_addr_counter;
+    logic [1:0] write_byte_counter;
+    logic [2:0] write_bit_counter;
+    logic [1:0] write_phase_counter;
+
+
     always_comb
     begin
-        if (reset) begin
-        addr_or_data = 0; // sets it to read only the addr
-        end
+
         next_state = s_idle;
         
-        scl = 1; 
-        //sda = 0;, probably cant assign?
-        reg_addr = 8'h0;
-        reset = 0;
+        //initalize counters
+        rom_addr_counter = 7'd0;
+        write_byte_counter = 2'd0;
+        write_bit_counter = 3'd7;
+        write_phase_counter = 2'd0;
 
+        scl = 1'bz;
+        sda = 1'bz;
+
+        //state descriptions
         unique case(curr_state)
             s_start:
             begin
-                sda = 0;
+                scl = 1'bz;
+                sda = 1'b0;
             end
-            s_write_7:
+            s_stop:
             begin
-                scl = 0;
-                if (~addr_or_data)begin
-                sda = reg_addr[7];
-                end
-                else begin
-                sda = reg_data[7];
-                end
+                scl = 1'bz;
+                sda = 1'bz;
+                rom_addr_counter = rom_addr_counter + 7'd1;
+                write_byte_counter = 2'd0;
             end
-            s_write_7_2:
+            s_ack:
+                write_bit_counter = 3'd7;
+                write_byte_counter = write_byte_counter + 2'd1;
+            s_write:
             begin
-                scl = 1;
+                case(write_phase_counter)
+                    2'd0:
+                    begin
+                        scl = 1'b0;
+                        write_phase_counter = write_phase_counter + 2'd1;
+                    end
+                    2'd1:
+                        case (write_byte_counter)
+                            2'd0:
+                                sda = addr_42[write_bit_counter];
+                            2'd1:
+                                sda = reg_addr[write_bit_counter];
+                            2'd2:
+                                sda = reg_data[write_bit_counter];
+                            default: 
+                                sda = addr_42[write_bit_counter];
+                        endcase
+                        write_phase_counter = write_phase_counter + 2'd1;
+                    2'd2:
+                    begin
+                        scl = 1'bz;
+                        write_phase_counter = 2'b0;
+                        write_bit_counter = write_bit_counter - 3'd1;
+                    end
+                    default:
+                        scl = 1'bz;
+                endcase
             end
-            s_write_7_3:
+            s_idle:
             begin
-                scl = 0;
-            end
-
-            s_write_6:
-            begin
-                scl = 0;
-                if (~addr_or_data)begin
-                sda = reg_addr[6];
-                end
-                else begin
-                sda = reg_data[6];
-                end
-            end
-            s_write_6_2:
-            begin
-                scl = 1;
-            end
-            s_write_6_3:
-            begin
-                scl = 0;
-            end
-
-            s_write_5:
-            begin
-                scl = 0;
-                if (~addr_or_data)begin
-                sda = reg_addr[5];
-                end
-                else begin
-                sda = reg_data[5];
-                end
-            end
-            s_write_5_2:
-            begin
-                scl = 1;
-            end
-            s_write_5_3:
-            begin
-                scl = 0;
-            end
-
-
-            s_write_4:
-            begin
-                scl = 0;
-                if (~addr_or_data)begin
-                sda = reg_addr[4];
-                end
-                else begin
-                sda = reg_data[4];
-                end
-            end
-            s_write_4_2:
-            begin
-                scl = 1;
-            end
-            s_write_4_3:
-            begin
-                scl = 0;
-            end
-
-
-            s_write_3:
-            begin
-                scl = 0;
-                if (~addr_or_data)begin
-                sda = reg_addr[3];
-                end
-                else begin
-                sda = reg_data[3];
-                end
-            end
-            s_write_3_2:
-            begin
-                scl = 1;
-            end
-            s_write_3_3:
-            begin
-                scl = 0;
-            end
-
-
-            s_write_2:
-            begin
-                scl = 0;
-                if (~addr_or_data)begin
-                sda = reg_addr[2];
-                end
-                else begin
-                sda = reg_data[2];
-                end
-            end
-            s_write_2_2:
-            begin
-                scl = 1;
-            end
-            s_write_2_3:
-            begin
-                scl = 0;
-            end
-
-
-            s_write_1:
-            begin
-                scl = 0;
-                if (~addr_or_data)begin
-                sda = reg_addr[1];
-                end
-                else begin
-                sda = reg_data[1];
-                end
-            end
-            s_write_1_2:
-            begin
-                scl = 1;
-            end
-            s_write_1_3:
-            begin
-                scl = 0;
-            end
-
-            s_write_0:
-            begin
-                scl = 0;
-                if (~addr_or_data)begin
-                sda = reg_addr[0];
-                end
-                else begin
-                sda = reg_data[0];
-                end
-            end
-            s_write_0_2:
-            begin
-                scl = 1;
-            end
-            s_write_0_3:
-            begin
-                scl = 0;
-            end
-
-
-
-
-
-            
+                scl = 1'bz;
+                sda = 1'bz;
+                rom_addr_counter = 7'd0;
+                write_byte_counter = 2'd0;
+            end        
         endcase
 
+
+        //transitions
         case (curr_state)
-            s_idle: if (start_fsm) begin
-                next_state = s_start;
-            end else begin
-                next_state = s_idle;
-            end
-            s_start: if (!sda) begin
-                next_state = s_write_7;
-            end
-            s_write_7: begin
-                next_state = s_write_7_2; 
-            end
-            s_write_7_2: begin
-                next_state = s_write_7_3;
-            end
-            s_write_7_3: begin
-                next_state = s_write_6;
-            end
-            s_write_6: begin
-                next_state = s_write_6_2; 
-            end
-            s_write_6_2: begin
-                next_state = s_write_6_3;
-            end
-            s_write_6_3: begin
-                next_state = s_write_5;
-            end
-            s_write_5: begin
-                next_state = s_write_5_2; 
-            end
-            s_write_5_2: begin
-                next_state = s_write_5_3;
-            end
-            s_write_5_3: begin
-                next_state = s_write_4;
-            end
-            s_write_4: begin
-                next_state = s_write_4_2; 
-            end
-            s_write_4_2: begin
-                next_state = s_write_4_3;
-            end
-            s_write_4_3: begin
-                next_state = s_write_3;
-            end
-            s_write_3: begin
-                next_state = s_write_3_2; 
-            end
-            s_write_3_2: begin
-                next_state = s_write_3_3;
-            end
-            s_write_3_3: begin
-                next_state = s_write_2;
-            end
-            s_write_2: begin
-                next_state = s_write_2_2; 
-            end
-            s_write_2_2: begin
-                next_state = s_write_2_3;
-            end
-            s_write_2_3: begin
-                next_state = s_write_1;
-            end
-            s_write_1: begin
-                next_state = s_write_1_2; 
-            end
-            s_write_1_2: begin
-                next_state = s_write_1_3;
-            end
-            s_write_1_3: begin
-                next_state = s_write_0;
-            end
-            s_write_0: begin
-                next_state = s_write_0_2; 
-            end
-            s_write_0_2: begin
-                next_state = s_write_0_3;
-            end
-            s_write_0_3: begin
-                next_state = s_ack;
-            end
-
-
-            s_ack: if (!sda) begin
-                addr_or_data = ~addr_or_data;
-                next_state = s_write_0;
-
-            end else begin
-                next_state = s_idle;
-            end
+            s_idle: 
+                next_state = start_fsm ? s_start : s_idle;
+            s_start:
+                next_state = s_write;
+            s_write:
+                next_state = (write_bit_counter == 3'd0 && write_phase_counter == 2'd0) ? s_ack : s_write;
+            s_ack:
+                next_state = (write_byte_counter == 2'd2 || sda == 1'bz) ? s_stop : s_write;    //may need to use sda_in rather than directly sda for ACK/NACK
+            s_stop:
+                next_state = (rom_addr_counter == 7'd72 || sda == 1'bz) ? s_idle : s_start;     //may need to use sda_in rather than directly sda for ACK/NACK
 
         endcase
     end
 
-    always_ff @(posedge clk)
+    always_ff @(posedge xclk)
     begin
         curr_state <= next_state;
     end
 
      OV7670_config_rom camera_rom(
         .clk(clk),
-        .addr(addr),
+        .addr(rom_addr_counter),
         .dout(dout)
     )
 
